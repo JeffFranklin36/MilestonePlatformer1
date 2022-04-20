a69c234bf3ed3787aa6fcaa915cfbd0b122b9529
Jeffrey <jeff.1001@live.com>
2022-04-19 20:43:41 -0500

    level and element constructors completed

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ let simpleLevelPlan = `
 ......##############..
 ......................`;
 
+
+
 class Level {
  constructor(plan) { // the argument that gets passed into plan is the string that defines a level
   let rows = plan.trim().split("\n").map(l => [...l]); // the string that gets passed in gets mapped into an array of arrays where each row of the string becomes an array exampleArray visualizes this
@@ -31,7 +33,7 @@ class State { // this keeps track of the state of a running game so events like 
  constructor(level, actors, status){
   this.level = level;
   this.actors = actors;
-  this.status = status
+  this.status = status;
  }
  static start(level){
   return new State(level, level.startActors, "playing");
@@ -41,7 +43,80 @@ class State { // this keeps track of the state of a running game so events like 
  }
 }
 
-let simpleLevel = new Level(simpleLevelPlan)
+class Vec {
+ constructor(x, y) {
+  this.x = x;
+  this.y = y;
+ }
+ plus(other) {
+  return new Vec(this.x + other.x, this.y + other.y);
+ }
+ times(factor) {
+  return new Vec(this.x * factor, this.y * factor);
+ }
+}
+
+class Player {
+ constructor(pos, speed) {
+  this.pos = pos;
+  this.speed = speed;
+ }
+ 
+ get type() { return 'player'; }
+
+ static create(pos) {
+  return new Player(pos.plus(new Vec(0, -0.5)),
+                       new Vec(0, 0));
+ }
+}
+
+Player.prototype.size = new Vec(0.8, 1.5);
+
+
+class Lava {
+ constructor(pos, speed, reset) {
+   this.pos = pos;
+   this.speed = speed;
+   this.reset = reset;
+ }
+ get type() { return 'lava'; }
+
+ static create(pos, ch) {
+  if (ch == "=") {
+   return new Lava(pos, new Vec(2,0));
+  } else if (ch == "|"){
+   return new Lava(pos, new Vec(0,2));
+  } else if (ch == "v"){
+   return new Lava(pos, new Vec(0,3))
+  }
+ }
+}
+
+Lava.prototype.size = new Vec(1,1);
+
+class Coin {
+ constructor(pos, basePos, wobble) {
+  this.pos = pos;
+  this.basePos = basePos;
+  this.wobble = wobble;
+ }
+ get type() { return "coin"; }
+
+ static create(pos) {
+  let basePos = pos.plus(new Vec(0.2, 0.1));
+  return new Coin(basePos, basePos,
+                  Math.random() * Math.PI *2);
+ }
+}
+
+Coin.prototype.size = new Vec(0.6, 0.6);
+
+const levelChars = {
+ ".": "empty", "#": "wall", "+": "lava", "@": Player, "o": Coin, "=": Lava, "|": Lava, "v": Lava
+};
+
+let simpleLevel = new Level(simpleLevelPlan);
+console.log(`${simpleLevel.width} by ${simpleLevel.height}`);
 
 // let exampleArray = [
 //  ['.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'],
